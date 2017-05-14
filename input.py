703705f8a427728f44a5fb3d9a7c9c2d66d7d88b
dimitrisl <dlolis88@gmail.com>
2017-05-14 14:09:29 +0300

    fixed newline

--- input.py
+++ input.py
@@ -8,10 +8,9 @@ train = {}
 test = {}
 for root, directories, files in os.walk(train_path):
     for f in files:
-        print f
         name = f.replace(".txt", "")
         f1 = open(train_path+os.sep+str(f))
-        data = [line.split(" ") for line in f1.readlines()]
+        data = [map(int, line.split(" ")) for line in f1.readlines()]
         bias = numpy.array(numpy.ones(len(data)))
         if "train" in f:
             train[name] = numpy.column_stack((bias, data))
@@ -20,5 +19,4 @@ for root, directories, files in os.walk(train_path):
         f1.close()
 
 
-print train["train0"][0]
-print test["test0"][0]
+print train["train0"][0], len(train["train0"][0])
