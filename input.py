8cc0ae5da91f2e7f8f3cae8ee31f90ae1607afe9
vasiapars <vasiliki.chira@gmail.com>
2017-05-15 16:24:57 +0300

    gradderivatives method

--- input.py
+++ input.py
@@ -5,12 +5,12 @@ import random
 train_path = os.path.join(os.getcwd(), "mnisttxt")
 
 
-def getweights(nm, M):
-    weights1 = 0.1 * numpy.random.randn(len(train[nm][0]), M)
+def getweights(nm, M):  # Random initialization of weights
+    weights1 = 0.1 * numpy.random.randn(len(X[nm][0]), M)
     weights2 = 0.1 * numpy.random.randn(M+1, 10)
     return weights1, weights2
 
-train = {}
+X = {}  # Input training data
 test = {}
 labels = {}
 for root, directories, files in os.walk(train_path):
@@ -20,18 +20,19 @@ for root, directories, files in os.walk(train_path):
         data = [map(int, line.split(" ")) for line in f1.readlines()]
         bias = numpy.array(numpy.ones(len(data)))
         if "train" in f:
-            train[name] = numpy.column_stack((bias, data))
+            X[name] = numpy.column_stack((bias, data))
             array = [0 for i in range(10)]
             label = int(re.search(r'\d+', f).group())
             array[label] = 1
-            labels[label] = [array for i in range(len(train[name]))]
+            labels[label] = [array for i in range(len(X[name]))]
         else:
             test[name] = numpy.column_stack((bias, data))
         f1.close()
 temp = []
 for i in range(10):
     temp.extend(labels[i])
-y_labels = numpy.array(temp)
-M = random.choice([100, 200, 300, 400, 500])
+T = numpy.array(temp)  # True labels of Training Examples
+M = random.choice([100, 200, 300, 400, 500])  # Number of units in the hidden layer
 
-weight1, weight2 = getweights("train0", M)
+W1, W2 = getweights("train0", M)  # Arrays of Weights in layer 1 and 2
+
--- tools.py
+++ tools.py
@@ -1,18 +1,29 @@
 import numpy
 
-def activationFunction(activationFunctionName,inputHL):
+
+def activationFunction(activationFunctionName, inputHL):
    
-    if activationFunctionName=="logSoftPlus":
-        result=numpy.log(1+numpy.exp(inputHL))
+    if activationFunctionName == "logSoftPlus":
+        result = numpy.log(1+numpy.exp(inputHL))
+
+    elif activationFunctionName == "tanh":
+        result = (numpy.exp(inputHL)-numpy.exp(-inputHL))/(numpy.exp(inputHL)+numpy.exp(-inputHL))
         
-    
-    elif activationFunctionName=="tanh":
-        result=(numpy.exp(inputHL)-numpy.exp(-inputHL))/(numpy.exp(inputHL)+numpy.exp(-inputHL))
-        
-    elif activationFunctionName=="cosine":
-        result=numpy.cos(inputHL)
+    elif activationFunctionName == "cosine":
+        result = numpy.cos(inputHL)
              
     return result
 
+
 def softmax(inputX):
     return numpy.exp(inputX) / numpy.sum(numpy.exp(inputX), axis=0)
+
+
+def gradderivatives(actFunction, z):
+    if actFunction == "logSoftPlus":
+        result = (numpy.exp(z)/numpy.exp(z)+1)
+    elif actFunction == 'tanh':
+        result = 1 - (numpy.square(numpy.exp(z)-numpy.exp(-z))/numpy.square(numpy.exp(z)+numpy.exp(-z)))
+    elif actFunction == 'cosine':
+        result = -numpy.sin(z)
+    return result
