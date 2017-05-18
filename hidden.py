92f75ed2e5ca4babf4361b92227b6a49b17c4537
vasiapars <vasiliki.chira@gmail.com>
2017-05-18 20:23:41 +0300

    costfunction1

--- hidden.py
+++ hidden.py
@@ -4,14 +4,15 @@ from tools import activationfunction, softmax
 
 
 X, T, W1, W2 = inputlayer()
-#Calculate Z1
-Z1 = X.dot(W1.T)
+def hiddenlayer():
+    #Calculate Z1
+    Z1 = X.dot(W1.T)
 
-#Call activation function
-H = activationfunction("tanh", Z1)
-A2 = numpy.c_[numpy.ones((H.shape[0], 1)), H]
-#Calculate Z2
-Z2 = A2.dot(W2.T)
+    #Call activation function
+    H = activationfunction("tanh", Z1)
+    A2 = numpy.c_[numpy.ones((H.shape[0], 1)), H]
+    #Calculate Z2
+    Z2 = A2.dot(W2.T)
 
-Y = softmax(Z2)
-print Y
+    Y = softmax(Z2)
+    return Y
--- output.py
+++ output.py
@@ -1,9 +1,23 @@
 import numpy as np
 from tools import softmax
+from InputLayer import inputlayer
+from hidden import hiddenlayer
 
 
-def costFunction(W2, Z2, reg, T):
-    A3 = Z2.dot(W2.T)
-    Y = softmax(A3)
-    E = np.log(Y).T.dot(T)-reg
+def sumnorm(W1, W2):
+    squares1 = np.sum(np.square(W1), axis=0)
+    sum1 = np.sum(squares1)
+    squares2 = np.sum(np.square(W2), axis=0)
+    sum2 = np.sum(squares2)
+    return sum1+sum2
+
+
+def costFunction(W1, W2, Y, T, reg=1):
+    E = np.sum(np.sum(np.log(Y).T.dot(T), axis=1))-(reg/2)*sumnorm(W1, W2)
     return E
+
+X, T, W1, W2 = inputlayer()
+Y = hiddenlayer()
+
+W = sumnorm(W1, W2)
+print costFunction(W1, W2, Y, T)
