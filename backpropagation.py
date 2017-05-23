a10b3678ecb58eda9920d6aad42b0e6378066bd4
ChrisChatz <chrischatzilenas@gmail.com>
2017-05-23 22:57:05 +0300

    add grad_weights_w1

--- backpropagation.py
+++ backpropagation.py
@@ -8,12 +8,15 @@ def error_output(Y, T):
     return delta_out
 
 
-def out_grad_weights(Z2, W2, d_out, reg):
+def out_grad_weights_w2(Z2, W2, d_out, reg):
 
     UpdW2 = Z2.T.dot(d_out)+reg*W2# Z is the output of the hidden layer and d_out the error_output
     return UpdW2
 
-
+def out_grad_weights_w1(Z2, W2, d_out, reg, W1 ,X, z1 ,actFunction):
+    UpdW1=Z2.T.dot(W2).dot(gradderivH(actFunction, z1)).dot(X)-reg*W1
+    return UpdW1
+    
 def error_hidden(W2, d_out, Z1, W1, reg):
     #
     pass
