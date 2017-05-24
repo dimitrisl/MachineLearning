07d9cfbb591811f6ea738897a95c5bc5ad44d8f9
vasiapars <vasiliki.chira@gmail.com>
2017-05-24 14:35:42 +0300

    modified backpropagate

--- backpropagation.py
+++ backpropagation.py
@@ -4,19 +4,21 @@ from tools import gradderivH
 
 
 def error_output(Y, T):
-    delta_out = T-Y# gradient error in output layer or layer 3
-    return delta_out
+     return T-Y# gradient error in output layer or layer 3
 
 
 def out_grad_weights_w2(Z2, W2, d_out, reg):
-
-    UpdW2 = Z2.T.dot(d_out)+reg*W2# Z is the output of the hidden layer and d_out the error_output
+    UpdW2 = Z2.T.dot(d_out)+reg.dot(W2) # Z is the output of the hidden layer and d_out the error_output
     return UpdW2
 
-def out_grad_weights_w1(Z2, W2, d_out, reg, W1 ,X, z1 ,actFunction):
-    UpdW1=W2.T.dot(d_out).dot(gradderivH(actFunction, z1)).dot(X)-reg*W1
-    return UpdW1
-    
-def error_hidden(W2, d_out, Z1, W1, reg):
-    #
-    pass
+def out_grad_weights_w1(W2, d_out, W1 ,X, Z1 ,actFunction, reg):
+    d_1 = np.multiply(gradderivH(actFunction, Z1), d_out.dot(W2.T))
+    updW1 = X.T.dot(d_1) - reg.dot(W1)
+    return updW1
+
+
+def backpropagate():
+    d_out= error_output(Y, T)
+    UpdW2 = out_grad_weights_w2(Z2, W2, d_out, reg=1)
+    UpdW1 = out_grad_weights_w1(W2, d_out, W1, X, Z1, actFunction, reg=1)
+    return UpdW2, UpdW1
