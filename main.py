7e3cafbefad0b571e55698644c11f09ea9f9e998
ChrisChatz <chrischatzilenas@gmail.com>
2017-06-08 23:30:22 +0300

    add epochs

--- main.py
+++ main.py
@@ -4,17 +4,27 @@ from hidden import hiddenlayer
 from output import outputlayer
 from backpropagation import backpropagate
 
-threshold = 10
+threshold = 0
 
 activation_function = raw_input("Please choose one of the activation functions: 1)logSoftPlus 2)tanh 3)cosine : ")
 M = input("Choose the number of activation units from : 100, 200, 300, 400, 500 : ")
 
+epoch = 1
+print "-------------epoch %s-------------" %epoch
 X, T, W1, W2 = inputlayer(M)
 Z1, A2 = hiddenlayer(X, W1, activation_function)
 Y, Z2 = outputlayer(A2, W2)
 E = costFunction(Y, T, W1, W2)
-UpW1, UpW2 = backpropagate(X, Y, T, Z1, A2,  W1, W2, activation_function)
-#difference = 0
+UpW2, UpW1 = backpropagate(X, Y, T, Z1, A2,  W1, W2, activation_function)
+difference = E
 
-
-#difference = cost - difference
+while difference!=threshold: 
+    epoch+=1
+    print "-------------epoch %s-------------" %epoch
+    Z1, A2 = hiddenlayer(X, UpW1, activation_function)
+    Y, Z2 = outputlayer(A2, UpW2)
+    E = costFunction(Y, T, UpW1, UpW2)
+    UpW2, UpW1 = backpropagate(X, Y, T, Z1, A2,  UpW1, UpW2, activation_function)
+    difference = difference - E
+    
+print E
